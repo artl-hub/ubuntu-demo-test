72cb498252a4d9e594ef1b428fb047d7518fb9e4
artl <artemlarichev.v@gmail.com>
2025-07-04 13:38:38 +0200

    update environvet capabilities

--- src/test/java/demo/GoogleTest.java
+++ src/test/java/demo/GoogleTest.java
@@ -3,6 +3,7 @@ package demo;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.*;
 
@@ -10,12 +11,24 @@ public class GoogleTest {
 
     @BeforeAll
     static void setup() {
-//        Configuration.remote = "http://localhost:4444/wd/hub";
+        String remoteUrl = System.getProperty("remoteUrl");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        if (remoteUrl != null && !remoteUrl.isEmpty()) {
+            Configuration.remote = remoteUrl;
+            capabilities.setCapability("enableVNC", true);
+            // capabilities.setCapability("enableVideo", true);
+            Configuration.browserVersion = "120.0";
+        }
+
         Configuration.browser = "chrome";
         Configuration.browserSize = "1280x1024";
         Configuration.timeout = 10000;
-//        Configuration.browserCapabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
     }
+
 
     @Test
     void openGoogleAndWait() throws InterruptedException {
